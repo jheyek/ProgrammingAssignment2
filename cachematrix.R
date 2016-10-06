bc07b41591c94a4ef7fc98259ffaf3a0f6e93acb
jheyek <joseph.killoran@gmail.com>
2016-10-05 22:44:28 -0400

    A few small modifications

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,19 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
 makeCacheMatrix <- function(x = matrix()) {
   m = matrix()
   set <- function(y){
-      x <<- y
-      m <<- NULL
+      if (is.matrix(x) && is.matrix(y) && dim(x) == dim(y) && all(x == y) ) {
+        message("An idential matrix is already set.")
+      }else{
+        if(is.matrix(y)){
+          x <<- y
+          m <<- NULL
+        }else{
+          message("Input argument to this fuction must be a matrix.")
+        }
+      }
   }
   get <- function() x
   setinverse <- function(invM) m <<- invM
@@ -16,7 +23,6 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   m <- x$getinverse()
